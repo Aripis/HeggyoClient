3e0cae53d6ce7c3f43ac34cbfa60d00845fe4518
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2020-10-16 21:03:56 +0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import styles from 'styles/Index.module.scss';
+
+const Index = () => {
+    return (
+        <Container maxWidth='sm'>
+            <Box my={4}>
+                <Typography variant='h4' component='h1' gutterBottom>
+                    Next.js with TypeScript example
+                </Typography>
+            </Box>
+        </Container>
+    );
+};
+
+export default Index;
