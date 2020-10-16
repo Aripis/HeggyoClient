b4f2b561b89fab576dff0816ff062ae13f3f923d
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2020-10-17 02:12:42 +0300

    [Update] Added .env, configured MUI and TS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import { useEffect } from 'react';
+import { Container, Typography } from '@material-ui/core';
 import styles from 'styles/Index.module.scss';
 
 const Index = () => {
+    useEffect(() => {
+        fetch(process.env.NEXT_PUBLIC_API_URL)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }, []);
+
     return (
         <Container maxWidth='sm' className={styles.content}>
-            <Box my={4}>
-                <Typography variant='h4' component='h1' gutterBottom>
-                    Next.js with TypeScript example
-                </Typography>
-            </Box>
+            <Typography variant='h4' component='h1' gutterBottom>
+                Next.js with TypeScript example
+            </Typography>
         </Container>
     );
 };
